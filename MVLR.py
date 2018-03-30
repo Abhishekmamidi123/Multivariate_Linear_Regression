147fa5d6e54828ddf95f8288f6d1606aa1debca4
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-03-30 10:30:41 +0530

    Data - Final

---
+++ MVLR.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn import linear_model
+
+filename = 'data/brainhead.csv'
+
+dataTrain = pd.read_csv("data/brainhead.csv")
+# dataTest = pd.read_csv("dataTest.csv")
+
+print dataTrain
